df496ac9572b8495aa9bd12e2ac695ecd12d941c
HP <gayratjonuzbek07@gmail.com>
2023-04-28 14:02:03 +0500

    xx

--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -3,15 +3,36 @@ package com.example.demo;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
-@RestController
-@RequestMapping("/api")
-public class Controller {
 
-    @GetMapping("/hello")
-    public String hello(){
-        return "Hello";
+public class Controller  extends TelegramLongPollingBot {
+
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage()){
+            SendMessage sendMessage=new SendMessage();
+            sendMessage.setChatId(update.getMessage().getChatId());
+            sendMessage.setText(update.getMessage().getText());
+            try {
+                execute(sendMessage);
+            } catch (TelegramApiException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 
+    @Override
+    public String getBotUsername() {
+        return "t.me/prayerTimeUZ_bot";
+    }
 
+    @Override
+    public String getBotToken() {
+        return "6280677362:AAHYqcGMnmYbFwjrPRgua5McHzTvNeyGW_4";
+    }
 }
--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,11 +2,22 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
+
+		try {
+			TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+			telegramBotsApi.registerBot(new Controller());
+		} catch (TelegramApiException e) {
+			e.printStackTrace();
+		}
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
